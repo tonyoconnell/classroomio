c8aff4bbaab0af9961f761ae15d0e4510e423aa6
Anthony O'Connell <tony@one.ie>
2024-06-09 17:04:36 +0700

    Cloudflare

--- apps/dashboard/svelte.config.js
+++ apps/dashboard/svelte.config.js
@@ -1,20 +1,22 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
-import vercelAdapter from '@sveltejs/adapter-vercel';
-import path from 'path';
-
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess({})],
-
-  kit: {
-    adapter: vercelAdapter(),
-    alias: {
-      $lib: path.resolve('./src/lib'),
-      $mail: path.resolve('./src/mail')
-    }
-  }
+	kit: {
+		adapter: adapter({
+			// See below for an explanation of these options
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		})
+	},
+	preprocess: vitePreprocess()
 };
-
 export default config;
