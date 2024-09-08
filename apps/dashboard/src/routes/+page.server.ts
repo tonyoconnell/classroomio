c9ea639119dceb9e1d11dc64b317b2b0fb9fd8bf
Anthony O'Connell <tony@one.ie>
2024-09-08 17:44:55 +0700

    Redirect to Learn

--- apps/dashboard/src/routes/+page.server.ts
+++ apps/dashboard/src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export function load() {
-    throw redirect(307, 'https://rebuildyou.rebuildyou.co/');
+    throw redirect(307, 'https://learn.rebuildyou.co/');
 }
